295cf61f23ccfd361c15239531e38be9658482f8
Personal Profile <davina.leong.sy@gmail.com>
2021-08-28 15:32:14 +0800

    Render skills

---
+++ js/main.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+  renderSkills($(".skills-programming"), 5);
+  renderSkills($(".skills-design"), 3);
+  renderSkills($(".skills-languages"), 2);
+});
+
+function generateRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+function ratingHtml() {
+  const max = 5;
+  const rating = generateRandomInt(max);
+  const remaining = max - rating;
+
+  let activeHtml = '';
+  for (let i = 0; i < rating; i++) {
+    activeHtml += '<span class="active"></span>';
+  }
+
+  let remainingHtml = '';
+  for (let i = 0; i < remaining; i++) {
+    remainingHtml += '<span></span>';
+  }
+
+  return '<div class="rating">' + activeHtml + remainingHtml + '</div>';
+}
+
+function renderSkills(element, count = 0){
+  console.log('render skills');
+  for (let i = 0; i < count; i++) {
+    const rating = ratingHtml();
+    element.append(`
+    <div>
+      <p>Skill / Skill</p>
+      ${rating}
+    </div>
+    `);
+  }
+}
